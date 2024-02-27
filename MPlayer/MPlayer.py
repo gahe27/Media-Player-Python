fe7996d31f45663bc1514db67550487123d0b094
Gabriel Henrique <gahetiete27@outlook.com>
2024-02-27 15:37:15 -0300

    Media Player Python

---
+++ MPlayer/MPlayer.py
@@ -0,0 +1,115 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QStyle, QSlider, QFileDialog
+from PyQt5.QtGui import QIcon, QPalette
+from PyQt5.QtCore import Qt, QUrl
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
+from PyQt5.QtMultimediaWidgets import QVideoWidget
+import sys
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowIcon(QIcon("player.ico"))
+        self.setWindowTitle("PyPlayer")
+        self.setGeometry(350, 100, 700, 500)
+
+        p = self.palette()
+        p.setColor(QPalette.Window, Qt.GlobalColor.blue)
+        self.setPalette(p)
+
+        self.create_player()
+
+    def create_player(self):
+        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+
+        videowidget = QVideoWidget()
+
+        self.openBtn = QPushButton("Abrir Video")
+        self.openBtn.clicked.connect(self.open_file)
+
+        self.playBtn = QPushButton()
+        self.playBtn.setEnabled(False)
+        self.playBtn.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+
+        self.playBtn.clicked.connect(self.play_video)
+
+        self.slider = QSlider(Qt.Horizontal)
+        self.slider.setRange(0,0)
+        self.slider.sliderMoved.connect(self.set_position)
+
+        self.btn = QPushButton()
+        self.btn.setEnabled(False)
+        self.btn.setIcon(self.style().standardIcon(QStyle.SP_MediaVolume))
+
+        self.volSlider = QSlider(Qt.Horizontal)
+        self.volSlider.setRange(0, 100)
+        self.volSlider.setValue(100)
+        self.volSlider.valueChanged.connect(self.set_volume)
+
+        hbox = QHBoxLayout()
+        hbox.setContentsMargins(0, 0, 0, 0)
+
+        hbox.addWidget(self.openBtn)
+        hbox.addWidget(self.playBtn)
+        hbox.addWidget(self.slider)
+        hbox.addWidget(self.btn)
+        hbox.addWidget(self.volSlider)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(videowidget)
+        vbox.addLayout(hbox)
+
+        self.mediaPlayer.setVideoOutput(videowidget)
+
+        self.setLayout(vbox)
+
+        self.mediaPlayer.stateChanged.connect(self.mediastate_changed)
+        self.mediaPlayer.positionChanged.connect(self.position_changed)
+        self.mediaPlayer.durationChanged.connect(self.duration_changed)
+
+
+    def open_file(self):
+        filename, _ = QFileDialog.getOpenFileName(self, "Open Video")
+
+        if filename != '':
+            self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(filename)))
+            self.playBtn.setEnabled(True)
+
+
+    def play_video(self):
+        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+            self.mediaPlayer.pause()
+
+        else:
+            self.mediaPlayer.play()
+
+    def mediastate_changed(self, state):
+        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+            self.playBtn.setIcon(self.style().standardIcon(QStyle.SP_MediaPause))
+
+        else:
+            self.playBtn.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+
+    def position_changed(self, position):
+        self.slider.setValue(position)
+
+    def duration_changed(self, duration):
+        self.slider.setRange(0, duration)
+
+    def set_position(self, position):
+        self.mediaPlayer.setPosition(position)
+
+    def set_volume(self, volume):
+        if volume == 0:
+            self.btn.setIcon(self.style().standardIcon(QStyle.SP_MediaVolumeMuted))
+        else:
+            self.btn.setIcon(self.style().standardIcon(QStyle.SP_MediaVolume))
+
+        self.mediaPlayer.setVolume(volume)
+
+
+
+app = QApplication(sys.argv)
+window = Window()
+window.show()
+sys.exit(app.exec_())
